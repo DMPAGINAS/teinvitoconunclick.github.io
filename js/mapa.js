6d2e01b99d3262e65fd6166e0d413044ec5d16f3
DMPAGINAS <138554240+DMPAGINAS@users.noreply.github.com>
2024-02-19 00:17:26 -0300

    mapa.js

--- js/mapa.js
+++ js/mapa.js
@@ -1,18 +1,12 @@
 
-let Coords = {lat: -38.558149, lng: -58.6694818};
-        let map;
-        let marker;
-        let mapDiv = document.getElementById('map')
-        
-        function initMap(){
-           
-             map = new google.maps.Map(mapDiv,{
-                zoom:15,
-                center: Coords,
-            });
-        
-        marker = new google.maps.Marker ({
-                position: Coords,
-                mar: map,
-            });
-        }
+function iniciarMap(){
+    var coord = {lat: -38.558149, lng: -58.6694818};
+    var map = new google.maps.Map(document.getElementById('map'),{
+        zoom:15,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
